e19e7df12942d976bd8205f97c472597693fa0cb
Abdullah <a@randmar.io>
2024-01-17 00:02:27 -0800

    Update package name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig ({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.jsx"),
-      name: "rampui",
+      name: "tester",
    		 
       fileName: "index",
     },
