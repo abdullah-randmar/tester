57d93744cc65023838b4c86241839114dcf745fc
Abdullah <a@randmar.io>
2024-01-17 00:00:15 -0800

    init

---
+++ src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <button>{children} + Hello, World!</button>
+  );
+}
